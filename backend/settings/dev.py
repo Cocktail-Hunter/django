8891152ea6912d139e579125bea96dc5a1129f5f
Spimy <officialspyme@gmail.com>
2020-11-02 21:07:40 +0400

    Add settings for dev environment

---
+++ backend/settings/dev.py
@@ -0,0 +1,20 @@
+from .base import *
+
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+
+# CORS_ORIGIN_WHITELIST = (
+#     'http://localhost:8080',
+#     'http://127.0.0.1:8080'
+# )
