30e8a6dbda3d8fd0a94a7d8a90dfee67c69adcdc
Spimy <officialspyme@gmail.com>
2020-12-07 23:12:50 +0400

    Import urls from api folder

--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -1,13 +1,8 @@
-from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView
-)
+from django.urls import path, include
 
 
 app_name = 'authentication'
 
 urlpatterns = [
-    path('login/', TokenObtainPairView.as_view(), name='token_pair'),
-    path('refresh/', TokenRefreshView.as_view(), name='token_refresh')
+    path('', include('apps.authentication.api.urls')),
 ]
