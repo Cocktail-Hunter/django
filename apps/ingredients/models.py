7ccd62f5bed4169933a3ea4f44a428d071b6225f
Spimy <officialspyme@gmail.com>
2020-12-10 21:52:34 +0400

    Add ingredient model

---
+++ apps/ingredients/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from apps.authentication.models import User
+
+
+class IngredientState(models.IntegerChoices):
+    PENDING = 0, 'Pending'
+    APPROVED = 1, 'Approved'
+    DENIED = 2, 'Denied'
+
+
+class Ingredient(models.Model):
+
+    cocktaildb_id = models.CharField(max_length=255, null=True, blank=True)
+    name = models.CharField(max_length=255)
+    state = models.PositiveSmallIntegerField(
+        default=IngredientState.PENDING, choices=IngredientState.choices
+    )
+    is_public = models.BooleanField(default=True)
+    added_by = models.ForeignKey(
+        User, null=True, blank=True, on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return str(self.name)
