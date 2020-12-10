7b6e0ca0f0c4cadb1f76bfbdf4aecad7256499ea
Spimy <officialspyme@gmail.com>
2020-12-10 23:06:32 +0400

    Filter ingredient to display the proper data

--- apps/ingredients/api/views.py
+++ apps/ingredients/api/views.py
@@ -1,15 +1,26 @@
+from django.db.models import Q
 from rest_framework.generics import ListCreateAPIView
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-from ..models import Ingredient
+from ..models import Ingredient, IngredientState
 from .serializers import IngredientSerializer
 
 
 class IngredientAPIView(ListCreateAPIView):
     permission_classes = (IsAuthenticatedOrReadOnly,)
-    queryset = Ingredient.objects.all()
 
     def get_serializer_class(self):
         if self.request.version == 'v1':
             return IngredientSerializer
         return IngredientSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        queryset = Ingredient.objects.filter(
+            Q(added_by__id=user.id) |
+            Q(public=True),
+            # and
+            Q(added_by__id=user.id) |
+            Q(state=IngredientState.APPROVED)
+        )
+        return queryset
