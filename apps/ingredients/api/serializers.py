c994049b3b54e2fb3e9fb5a4318811bec0fdfa5d
Spimy <officialspyme@gmail.com>
2020-12-10 22:44:36 +0400

    Refactor serializer to work with adding and listing ingredient

--- apps/ingredients/api/serializers.py
+++ apps/ingredients/api/serializers.py
@@ -3,17 +3,21 @@ from rest_framework import serializers
 from ..models import Ingredient, IngredientState
 
 
-class AddIngredientSerializer(serializers.ModelSerializer):
+class IngredientSerializer(serializers.ModelSerializer):
     '''
     Serialize data between JSON and Python readable dictionary format
     '''
+    state = serializers.SerializerMethodField('get_state')
+    added_by = serializers.SerializerMethodField('get_author_name')
+
     class Meta:
         '''
         Meta data for the serializer
         e.g. what model/table is this serializer for?
         '''
         model = Ingredient
-        fields = ('name', 'public')
+        fields = '__all__'
+        read_only_fields = ('cocktaildb_id', 'state', 'added_by')
         extra_kwargs = {
             'public': {'required': True}
         }
@@ -30,3 +34,9 @@ class AddIngredientSerializer(serializers.ModelSerializer):
             validated_data['state'] = IngredientState.APPROVED
 
         return super().create(validated_data)
+
+    def get_state(self, ingredient):
+        return ingredient.get_state_display()
+
+    def get_author_name(self, ingredient):
+        return ingredient.added_by.username
