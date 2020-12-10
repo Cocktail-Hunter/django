0b302cf147e7fcb4b788cd20ac2851e1f8f76724
Spimy <officialspyme@gmail.com>
2020-12-10 21:53:09 +0400

    Add serializer to parse data sent from frontend and validate it

---
+++ apps/ingredients/api/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+
+from ..models import Ingredient, IngredientState
+
+
+class AddIngredientSerializer(serializers.ModelSerializer):
+    '''
+    Serialize data between JSON and Python readable dictionary format
+    '''
+    class Meta:
+        '''
+        Meta data for the serializer
+        e.g. what model/table is this serializer for?
+        '''
+        model = Ingredient
+        fields = ('name', 'is_public')
+        extra_kwargs = {
+            'is_public': {'required': True}
+        }
+
+    def create(self, validated_data):
+        '''
+        Set the appropriate data that has not been passed through
+        the request body like 'added_by'
+        '''
+        validated_data['added_by'] = self.context.get('request').user
+
+        # Set the ingredient to the appropriate state
+        if not validated_data.get('is_public'):
+            validated_data['state'] = IngredientState.APPROVED
+
+        return super().create(validated_data)
