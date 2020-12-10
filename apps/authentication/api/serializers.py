2d9d2e87a54aae0f7a59376ffe4933c34d36fdcb
Spimy <officialspyme@gmail.com>
2020-12-10 19:28:02 +0400

    Return jwt tokens on user registration for instant login

--- apps/authentication/api/serializers.py
+++ apps/authentication/api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from ..models import User
 
@@ -8,27 +9,35 @@ class RegistrationSerializer(serializers.ModelSerializer):
     '''
     Serialize data between JSON and Python readable dictionary format
     '''
+    tokens = serializers.SerializerMethodField()
+
     class Meta:
         '''
         Meta data for the serializer
         e.g. what model/table is this serializer for?
         '''
         model = User
-        fields = ('username', 'email', 'password')
+        fields = ('username', 'email', 'password', 'tokens')
         extra_kwargs = {
             'password': {'write_only': True}
         }
 
-    def save(self):
+    def get_tokens(self, user):
+        token_pairs = RefreshToken.for_user(user)
+        tokens = {
+            "refresh": str(token_pairs),
+            "access": str(token_pairs.access_token)
+        }
+        return tokens
+
+    def create(self, validated_data):
         '''
-        Handle data once serializer is saved
+        Create user from serialized data from valid POST request
         '''
         user = User(
-            username=self.validated_data.get('username'),
-            email=self.validated_data.get('email')
+            username=validated_data.get('username'),
+            email=validated_data.get('email')
         )
-        password = self.validated_data.get('password')
-
-        user.set_password(password)
+        user.set_password(validated_data.get('password'))
         user.save()
         return user
