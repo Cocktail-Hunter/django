9fab4fa9bfefc821d7ffddc450d6f236e3f0732b
Spimy <officialspyme@gmail.com>
2020-12-14 15:12:56 +0400

    Allow only email to login

--- apps/authentication/backends.py
+++ apps/authentication/backends.py
@@ -3,27 +3,16 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import ModelBackend
 
 
-class EmailOrUsernameBackend(ModelBackend):
+class EmailBackend(ModelBackend):
     '''
-    Allow users to authenticate using case insensitive username
-    or using email which provides a way for users who forgot their
-    username to authenticate themselves
+    Allow login only using email and not username.
     '''
 
-    def authenticate(self, request, username=None, password=None, **kwargs):
-
+    def authenticate(self, request, email=None, password=None, **kwargs):
         user_model = get_user_model()
 
-        if username is None:
-            username = kwargs.get(user_model.USERNAME_FIELD)
-
-        case_insensitive_username_field = f'{user_model.USERNAME_FIELD}__iexact'
-
         try:
-            user = user_model.objects.get(
-                Q(**{case_insensitive_username_field: username}) |
-                Q(email__iexact=username)
-            )
+            user = user_model.objects.get(email__iexact=email)
             return user if user.check_password(password) else None
         except user_model.DoesNotExist:
             return None
