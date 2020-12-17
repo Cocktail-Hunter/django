2536b384c1e67194cd912d9756f76316353c17a7
Spimy <officialspyme@gmail.com>
2020-12-17 17:54:30 +0400

    Add url to specific ingredient for updating

--- apps/ingredients/api/urls.py
+++ apps/ingredients/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import IngredientAPIView
+from .views import IngredientAPIView, IngredientUpdateAPIView
 
 
 urlpatterns = [
     path('', IngredientAPIView.as_view(), name='ingredients-list-add'),
+    path('<pk>/', IngredientUpdateAPIView.as_view(), name='ingredients-update')
 ]
