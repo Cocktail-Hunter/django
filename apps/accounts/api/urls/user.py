283f6006f535e30dae20a0c195ce7fc26c600d39
Spimy <officialspyme@gmail.com>
2020-12-25 22:11:11 +0400

    Add user info endpoint url

--- apps/accounts/api/urls/user.py
+++ apps/accounts/api/urls/user.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from ..views.user import InventoryAPIView
+from ..views.user import InventoryAPIView, UserRetrieveAPIView
 
 
 urlpatterns = [
-    path('inventory/', InventoryAPIView.as_view(), name='token_pair')
+    path('inventory/', InventoryAPIView.as_view(), name='inventory'),
+    path('', UserRetrieveAPIView.as_view(), name='user')
 ]
