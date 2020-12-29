6dd40d83c20c5f3be3d8919d503e291e97d11e34
Spimy <officialspyme@gmail.com>
2020-12-29 16:48:34 +0400

    Add url for detail and update endpoint

--- apps/cocktails/api/urls.py
+++ apps/cocktails/api/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 
-from .views import CocktailAPIView, CocktailsAddAPIView
+from .views import CocktailAPIView, CocktailsAddAPIView, CocktailDetailUpdateAPIView
 
 urlpatterns = [
     path('', CocktailAPIView.as_view(), name='cocktails-list'),
     path('add/', CocktailsAddAPIView.as_view(), name='cocktails-add'),
+    path(
+        '<pk>/',
+        CocktailDetailUpdateAPIView.as_view(),
+        name='cocktails-detail-update'
+    ),
 ]
