f7ba3ee7faf9f73097daa1f24b0522e3618b66ac
Eray Chumak <eray_chumak@protonmail.com>
2020-12-30 19:45:48 +0000

    Allow all origins in dev (cors)

--- backend/settings/dev.py
+++ backend/settings/dev.py
@@ -13,8 +13,4 @@ DATABASES = {
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-
-CORS_ALLOWED_ORIGINS = (
-    'http://localhost:8080',
-    'http://127.0.0.1:8080'
-)
+CORS_ORIGIN_ALLOW_ALL = True
